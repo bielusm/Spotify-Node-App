84f8bbdb2a70f9c8801d79be8e2e7d46eb175a08
Mitchell Bielus <mitchellbielus@gmail.com>
2019-09-25 21:23:29 -0500

    Can now remove playlists

--- js/api.js
+++ js/api.js
@@ -9,112 +9,136 @@ class API {
 
   currrentPlayer() {
     return new Promise((resolve, reject) => {
-    fetch("https://api.spotify.com/v1/me/player", {
-        headers: {'Authorization': "Bearer " + this.bearerToken},
-        method: "GET"
-      })
-      .then(data => data.json())
-      .then((json) => {
-        if(json === "")
-          reject("Nothing is playing");
-        this.currentTrackUri = json.item.uri;
-        resolve(json.item);
-      })
-      .catch((error) => reject(error));
-  });
-}
-  getPlaylists(){
-    return new Promise((resolve,reject) => {
-    fetch('https://api.spotify.com/v1/me/playlists', {
-        headers: {'Authorization': "Bearer " + this.bearerToken},
-         method: "GET"
-      })
-      .then(data => data.json())
-      .then((json) => {
+      fetch("https://api.spotify.com/v1/me/player", {
+          headers: {
+            'Authorization': "Bearer " + this.bearerToken
+          },
+          method: "GET"
+        })
+        .then(data => data.json())
+        .then((json) => {
+          if (json === "")
+            reject("Nothing is playing");
+          this.currentTrackUri = json.item.uri;
+          resolve(json.item);
+        })
+        .catch((error) => reject(error));
+    });
+  }
+  getPlaylists() {
+    return new Promise((resolve, reject) => {
+      fetch('https://api.spotify.com/v1/me/playlists', {
+          headers: {
+            'Authorization': "Bearer " + this.bearerToken
+          },
+          method: "GET"
+        })
+        .then(data => data.json())
+        .then((json) => {
           resolve(json);
-      })
-      .catch(error=>{
-        reject(error);
-      });
-  });
-}
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
 
-  addPlaylistByID(ID) {
-    return new Promise((resolve,reject) => {
-    let tracks = [];
-    let param = "?fields=next,items(track(name,artists,external_urls,id,uri,album(name)))"
-    fetch(`https://api.spotify.com/v1/playlists/${ID}/tracks${param}`, {
-        headers: {'Authorization': "Bearer " + this.bearerToken},
-         method: "GET"
-      })
-      .then(data => data.json())
-      .then((json) => {
-        tracks.push(json.items);
-        const next = json['next'];
-        if (next != null)
-        {
-          this.getNextTracks(ID, next, tracks)
-          .then(response=>{
-          this.playlists.push({id: ID, tracks: tracks});
-            resolve(response);})
-          .catch(error=>reject(error));
+  addPlaylistByID(ID, name) {
+    return new Promise((resolve, reject) => {
+      if (this.playlists.length > 0 && this.playlists.find(element => element.id === ID)) {
+        resolve("already added");
+      } else {
+        let tracks = [];
+        let param = "?fields=next,items(track(name,artists,external_urls,id,uri,album(name)))"
+        fetch(`https://api.spotify.com/v1/playlists/${ID}/tracks${param}`, {
+            headers: {
+              'Authorization': "Bearer " + this.bearerToken
+            },
+            method: "GET"
+          })
+          .then(data => data.json())
+          .then((json) => {
+            tracks.push(json.items);
+            const next = json['next'];
+            if (next != null) {
+              this.getNextTracks(ID, next, tracks)
+                .then(response => {
+                  this.playlists.push({
+                    id: ID,
+                    tracks: tracks,
+                    name: name
+                  });
+                  resolve(response);
+                })
+                .catch(error => reject(error));
+            } else {
+              this.playlists.push({
+                id: ID,
+                tracks: tracks,
+                name: name
+              });
+              resolve("fetch finished");
+            }
+          })
+          .catch((error) => reject(error));
       }
-    else
-    {
-      this.playlists.push({id: ID, tracks: tracks});
-      resolve("fetch finished");
-    }
-    })
-      .catch((error) => reject(error));
     });
   }
 
   getNextTracks(ID, next, tracks) {
-    return new Promise((resolve,reject) => {
-    fetch(next,
-      {headers: {'Authorization': "Bearer " + this.bearerToken},
-      method: "GET"
-      })
-      .then(data => data.json())
-      .then((json) => {
+    return new Promise((resolve, reject) => {
+      fetch(next, {
+          headers: {
+            'Authorization': "Bearer " + this.bearerToken
+          },
+          method: "GET"
+        })
+        .then(data => data.json())
+        .then((json) => {
 
-        tracks.push(json.items);
+          tracks.push(json.items);
 
-        const next = json['next'];
-        if (next != null)
-          this.getNextTracks(ID, next, tracks)
-          .then(response=>resolve(response))
-          .catch(error=>reject(error));
-        else
-          return resolve("fetch finished");
-      })
-      .catch((error) => reject(error));
+          const next = json['next'];
+          if (next != null)
+            this.getNextTracks(ID, next, tracks)
+            .then(response => resolve(response))
+            .catch(error => reject(error));
+          else
+            return resolve("fetch finished");
+        })
+        .catch((error) => reject(error));
     })
   }
-  currentTrackInPlaylists(){
+  currentTrackInPlaylists() {
     let ids = [];
-    console.log(this);
     let index = 0;
     this.playlists.forEach(playlist => {
-      if(this.currentTrackInPlaylist(index))
-        ids.push(playlist.id);
+      if (this.currentTrackInPlaylist(index))
+        ids.push(playlist);
       index++;
     });
     return ids;
   }
 
-  currentTrackInPlaylist(index){
+  currentTrackInPlaylist(index) {
     let equal = false;
-      console.log(this.playlists);
-      console.log(index);
-      this.playlists[index].tracks.forEach(playlist =>{
-        playlist.forEach(subPlaylist => {
-        if(subPlaylist.track.uri === this.currentTrackUri)
-            equal = true;
+    this.playlists[index].tracks.forEach(playlist => {
+      playlist.forEach(subPlaylist => {
+        if (subPlaylist.track.uri === this.currentTrackUri)
+          equal = true;
       });
     });
     return equal;
   }
+
+  removeFromPlaylist(id) {
+    for (let index = 0; index < this.playlists.length; index++) {
+      if (this.playlists[index].id === id) {
+        this.playlists.splice(index, 1);
+        break;
+      }
+    }
+  }
 }
 
-export default API;
+  export default API;
--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const loginButton = document.querySelector("#login");
 const updateBtn = document.querySelector("#updateBtn");
 const savePlaylistBtn = document.querySelector("#savePlaylistBtn");
 
-let playlistIDs = [];
+let playlists = [];
 
 let hash = location.hash;
 hash = hash.replace("#", "?");
@@ -25,28 +25,28 @@ updateBtn.addEventListener('click', () => {
     .then(currTrack => {
       let str = currTrack.name + " by ";
       currTrack.artists.forEach(artist => {
-        str += artist.name;
+        str += " " + artist.name;
       });
       trackEl.innerHTML = str;
       let promises = [];
-      playlistIDs.forEach(id=>{
-        promises.push(api.addPlaylistByID(id));
+      playlists.forEach(playlist => {
+        promises.push(api.addPlaylistByID(playlist.id, playlist.name));
       });
       const inPlaylists = document.querySelector("#inPlaylists");
       inPlaylists.innerHTML = "Loading..."
       Promise.all(promises)
-      .then(() =>{
-        let ids = api.currentTrackInPlaylists();
-        let html = "In playlists:\n"
-        if(ids.length > 0)
-          ids.forEach(id => {
-            html += id + "\n";
-          });
-        else
-          html = "Not in playlist";
-        inPlaylists.innerHTML = html;
-      })
-      .catch(error=>console.log(error));
+        .then(() => {
+          const playlists = api.currentTrackInPlaylists();
+          let html = "In playlists:\n"
+          if (playlists.length > 0)
+            playlists.forEach(playlist => {
+              html += playlist.name + "\n";
+            });
+          else
+            html = "Not in playlist";
+          inPlaylists.innerHTML = html;
+        })
+        .catch(error => console.log(error));
 
     })
     .catch(error => console.log(error));
@@ -54,25 +54,34 @@ updateBtn.addEventListener('click', () => {
 
 getPlaylistsBtn.addEventListener('click', () => {
   playlistsDiv.classList.toggle("hide");
-  if(playlistsDiv.childElementCount === 0)
-  {
-  api.getPlaylists()
-    .then(playlists => {
-      playlists.items.forEach(playlist => {
-        const playlistBtnLabel = document.createElement("p");
-        playlistBtnLabel.classList.add("playlist");
-        playlistBtnLabel.innerText = playlist.name;
-        playlistBtnLabel.id = playlist.id;
-        playlistsDiv.appendChild(playlistBtnLabel);
+  if (playlistsDiv.childElementCount === 0) {
+    api.getPlaylists()
+      .then(playlists => {
+        playlists.items.forEach(playlist => {
+          const playlistBtnLabel = document.createElement("p");
+          playlistBtnLabel.classList.add("playlist");
+          playlistBtnLabel.innerText = playlist.name;
+          playlistBtnLabel.id = playlist.id;
+          playlistsDiv.appendChild(playlistBtnLabel);
+        })
       })
-    })
   }
 });
 
-
-playlistsDiv.addEventListener('click', e=>{
-  if(e.target.tagName === "P")
+//When the user clicks on a playlist it will become selected/unselected
+playlistsDiv.addEventListener('click', e => {
+  if (e.target.tagName === "P")
     e.target.classList.toggle("clicked");
-  if(e.target.classList.contains("clicked"))
-    playlistIDs.push(e.target.id);
+  if (e.target.classList.contains("clicked"))
+    playlists.push({
+      name: e.target.innerHTML,
+      id: e.target.id
+    });
+  else { //remove from api playlist and app playlist
+    for (let index = 0; index < playlists.length; index++) {
+      if (playlists[index].id === e.target.id)
+        playlists.splice(index, 1);
+    }
+    api.removeFromPlaylist(e.target.id);
+  }
 })
