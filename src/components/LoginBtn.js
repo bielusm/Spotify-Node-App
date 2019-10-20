0bea56d5ff78128fe3737bd0f0188f45b26ec1b0
Mitchell Bielus <mitchellbielus@gmail.com>
2019-10-19 23:42:39 -0500

    Changed login redirect to use non constant value

--- src/components/LoginBtn.js
+++ src/components/LoginBtn.js
@@ -4,7 +4,9 @@ export default class LoginBtn extends React.Component {
   login = () => {
     const scope =
       "playlist-modify-public%20user-read-playback-state%20playlist-read-private%20playlist-modify-private";
-    const url = `https://accounts.spotify.com/authorize?client_id=4252feb807d04ced962e15f346258957&response_type=token&redirect_uri=http://localhost:3000/index.html&scope=${scope}&show_dialog=false`;
+    let redirect_uri = window.location.href;
+    redirect_uri = redirect_uri.slice(0, redirect_uri.length - 1);
+    const url = `https://accounts.spotify.com/authorize?client_id=4252feb807d04ced962e15f346258957&response_type=token&redirect_uri=${redirect_uri}&scope=${scope}&show_dialog=false`;
     window.location.href = url;
   };
 
