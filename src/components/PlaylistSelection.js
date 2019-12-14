45c0a87a30b11d7968f8452e557cabcf65933ac8
Mitchell Bielus <mitchellbielus@gmail.com>
2019-12-14 13:47:04 -0600

    Updated table

--- src/components/PlaylistSelection.js
+++ src/components/PlaylistSelection.js
@@ -1,28 +1,34 @@
 import React from "react";
+import {
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Table
+} from "@material-ui/core";
 
 export const PlaylistSelection = props => (
   <>
     {props.showPlaylists && (
-      <div>
-        <h2 className="playlist-selection__header">Your playlists:</h2>
-        <div
-          id="playlist-selection"
-          className="playlist-selection__list"
-          onClick={props.selectPlaylist}
-        >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Your playlists</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody onClick={props.selectPlaylist}>
           {props.playlists.map(playlist => {
-            const plName = playlist.name.replace(/ /g, "-");
-            const selected = playlist.selected;
             let className = "playlist-selection__item";
-            if (selected) className += " active";
             return (
-              <div key={playlist.name} id={playlist.id} className={className}>
-                {playlist.name}
-              </div>
+              <TableRow hover selected={playlist.selected} key={playlist.name}>
+                <TableCell id={playlist.id} className={className}>
+                  {playlist.name}
+                </TableCell>
+              </TableRow>
             );
           })}
-        </div>
-      </div>
+        </TableBody>
+      </Table>
     )}
   </>
 );
